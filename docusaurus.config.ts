d84332959c21fe4fe66fb0ea7d91c46bd10c9ba7
uertyk_ <151397011+uertyk@users.noreply.github.com>
2024-09-19 19:34:48 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'HollowEngine-fan-docs',
+  tagline: 'Давайте изучать сюжеты!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
